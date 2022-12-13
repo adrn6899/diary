163a28b22c967a87f918c4b62243dcb64be015ba
Adrian Pusana <aguilaradrian423@gmail.com>
2022-12-13 16:28:18 +0800

    commit

--- public/js/notes.js
+++ public/js/notes.js
@@ -3,13 +3,14 @@
 
     function viewItem(){
         $('.btn-view-item').on('click', function(e){
-            alert("viewing item");
+            // console.log(e);
+            alert($(this).val());
         });
     }
 
     function deleteItem(){
         $('.btn-delete-item').on('click', function(e){
-            alert("deleting item");
+            alert($(this).val());
         });
     }
 
@@ -23,7 +24,7 @@
                 console.log(result);
                 $.each(result, function(index, value){
                     $('.item-row').append('<div class="column"><div class="card item-card"><img class="card-img-top" src="https://cdn.dribbble.com/users/308895/screenshots/2598725/no-results.gif" alt="Card image cap"><div class="card-body"><h5 class="card-title">' + 
-                    value.title + ' </h5><p class="card-text">'+ value.content +'</p></div><div class="card-footer"><div class="row"><div class="col"><button class="btn btn-primary btn-sm btn-view-item">VIEW</button></div><div class="col"><button class="btn btn-danger btn-sm btn-delete-item">DELETE</button></div></div></div></div></div>');
+                    value.title + ' </h5><p class="card-text">'+ value.content +'</p></div><div class="card-footer"><div class="row"><div class="col"><button class="btn btn-primary btn-sm btn-view-item" value="'+value.id+'">VIEW</button></div><div class="col"><button class="btn btn-danger btn-sm btn-delete-item"  value="'+value.id+'">DELETE</button></div></div></div></div></div>');
                 });
             },
             error: function(error){
